97c68291f6eee107787ce95edc51e87e59be916f
Danny Sung <danny@dannysung.com>
2021-03-14 21:27:24 -0700

    docker build script: now supports private registry

--- docker/build.swift
+++ docker/build.swift
@@ -7,6 +7,14 @@ import Rainbow          // @onevcat
 
 let SwiftVersions = ["5.0.3", "5.1.5", "5.2.5", "5.3.3"]
 
+/// Given a target swift version, what aliases should exist?
+let SwiftAliases = [
+    "5.3.3" : [ "5.3", "5", "latest" ],
+    "5.2.5" : [ "5.2" ],
+    "5.1.5" : [ "5.1" ],
+    "5.0.3" : [ "5.0" ]
+    ]
+
 struct BuildCommand: ParsableCommand {
     static var configuration = CommandConfiguration(
         commandName: "build",
@@ -21,26 +29,89 @@ struct BuildCommand: ParsableCommand {
     @Flag(name: [.customLong("enable-push")], help: "Push docker images")
     var enablePush: Bool = false
 
+    @Flag(name: [.customLong("enable-aliases")], help: "Tag and push convenience aliases")
+    var enableAliases: Bool = false
+    
+    @Flag(name: [.customLong("dry-run"), .customShort("n")], help: "Dry-run (print but do not execute commands)")
+    var enableDryRun: Bool = false // TODO: during development, set to true
+
+    @Option(name: [.customLong("registry")], help: "Specify a private registry (https://[user[:password]@]registry.url)")
+    var registryUrlString: String?
+    var registryUrl: URL?
+
+    @Option(name: [.customLong("registry-password")], help: "Registry password")
+    var registryPasswordFromArg: String?
+
+    @Flag(name: [.customLong("registry-password-stdin")], help: "Read registry password from stdin")
+    var enableReadRegistryPasswordFromStdin: Bool = false
+    
     mutating func run() throws {
-        print("verbose: \(verbose)")
-        print("enableBuild: \(enableBuild)")
-        print("enablePush: \(enablePush)")
-        let actions = CompositeAction([PrintAction()])
+        var registryPasswordFromStdin: String? = nil
+        
+        if let urlString = self.registryUrlString {
+            self.registryUrl = URL(string: urlString)
+        }
+        
+        if enableReadRegistryPasswordFromStdin {
+            print("Enter password: ")
+            let input = SwiftShell.main.stdin.lines()
+            registryPasswordFromStdin = input.first(where: { _ in
+                    return true
+            })
+        }
+        
+        let actions: SystemAction
+        
+        if enableDryRun {
+            actions = CompositeAction([PrintAction()])
+        } else if verbose {
+            actions = CompositeAction([PrintAction(), RealAction()])
+        } else {
+            actions = CompositeAction([RealAction()])
+        }
 
         for swiftVersion in SwiftVersions {
             let version = try! Version(swiftVersion)
             
-            print("Found version: \(version)")
-            
             let build = BuildSwiftCI(swiftVersion: swiftVersion, systemAction: actions)
             if enableBuild {
+                actions.phase("Build docker image")
                 try build.build()
             }
             
             if enablePush {
+                actions.phase("Push docker image to public registry")
                 try build.push()
             }
             
+            if enableAliases {
+                if let aliases = SwiftAliases[swiftVersion] {
+                    actions.phase("Create public aliases")
+                    for aliasVersion in aliases {
+                        try build.alias(version: swiftVersion, alias: aliasVersion)
+                    }
+                }
+            }
+            
+            // Support pushing to private registry
+            if let registryUrl = registryUrl {
+                let host = registryUrl.host!
+                
+                if let user = registryUrl.user,
+                   let password = registryPasswordFromStdin ?? registryPasswordFromArg ?? registryUrl.password {
+
+                    // TODO: Support --password-stdin
+                    print("Attempt to log in: \(user)  pass: \(password)")
+                    try actions.runAndPrint(command: "docker", "login", host, "-u", user, "-p", password)
+                }
+
+                actions.phase("Push docker image to private registry")
+
+                try actions.runAndPrint(command: "docker", "tag", build.dockerTag, "\(host)/\(build.dockerTag)")
+                
+                try build.push(host: host)
+
+            }
         }
     }
 
@@ -55,10 +126,13 @@ protocol CreateDocker {
     var systemAction: SystemAction { get set }
     var swiftVersion: String { get }
     var dockerTag: String { get }
-    
+
     func create(file: URL) throws
     func build() throws
     func push() throws
+    func push(host: String) throws
+    func alias(version: String, alias: String) throws
+    func alias(host: String, version: String, alias: String) throws
 }
 
 extension CreateDocker {
@@ -76,8 +150,49 @@ extension CreateDocker {
     }
     
     func push() throws {
-        try self.systemAction.runAndPrint(command: "docker", "push", self.dockerTag)
+        try self._push()
     }
+    
+    func push(host: String) throws {
+        try self._push(host: host)
+    }
+    
+    private func _push(host: String? = nil) throws {
+        let tag: String
+        if let host = host {
+            tag = "\(host)/\(self.dockerTag)"
+        } else {
+            tag = self.dockerTag
+        }
+        try self.systemAction.runAndPrint(command: "docker", "push", tag)
+    }
+    func alias(version: String, alias: String) throws {
+        try self._alias(version: version, alias: alias)
+    }
+    
+    func alias(host: String, version: String, alias: String) throws {
+        try self._alias(host: host, version: version, alias: alias)
+    }
+    
+    private func _alias(host: String? = nil, version: String, alias: String) throws {
+        let existingTag: String
+        let aliasTag: String
+        
+        let dockerBaseTag = self.dockerTag.components(separatedBy: ":")[0]
+        
+        if let host = host {
+            existingTag = "\(host)/\(dockerBaseTag):\(version)"
+            aliasTag = "\(host)/\(dockerBaseTag):\(alias)"
+        } else {
+            existingTag = "\(dockerBaseTag):\(version)"
+            aliasTag = "\(dockerBaseTag):\(alias)"
+        }
+        
+        try self.systemAction.runAndPrint(command: "docker", "tag", existingTag, aliasTag)
+
+        try self.systemAction.runAndPrint(command: "docker", "push", aliasTag)
+    }
+
 }
 
 // MARK: Build Swift CI
@@ -139,10 +254,11 @@ class BuildSwiftDev: CreateDocker {
 }
 
 
-// MARK: SystemAction
+// MARK: - SystemAction
 /// A protocol for high level operations we may perform on the system.
 /// The intent of this protocol is to make it easier to perform "dry-run" operations.
 protocol SystemAction {
+    func phase(_ string: String)
     func createDirectory(url: URL) throws
     func createFile(fileUrl: URL, content: String) throws
     func runAndPrint(path: String?, command: [String]) throws
@@ -168,6 +284,10 @@ extension SystemAction {
 
 /// Actually perform the function
 class RealAction: SystemAction {
+    func phase(_ string: String) {
+        // do nothing
+    }
+    
     func createDirectory(url: URL) throws {
         let fm = FileManager.default
         try fm.createDirectory(at: url, withIntermediateDirectories: true, attributes: nil)
@@ -191,12 +311,18 @@ class RealAction: SystemAction {
         if let path = path {
             context.currentdirectory = path
         }
-        try context.runAndPrint("echo", command)
+        let cmd = command.first!
+        var args = command
+        args.removeFirst()
+        try context.runAndPrint(cmd, args)
     }
 }
 
 /// Only print the actions
 class PrintAction: SystemAction {
+    func phase(_ string: String) {
+        print(" == Phase: \(string)".cyan.bold)
+    }
     func createDirectory(url: URL) throws {
         print(" > Creating directory at path: \(url.path)".bold)
     }
@@ -222,6 +348,11 @@ class CompositeAction: SystemAction {
         self.actions = actions
     }
     
+    func phase(_ string: String) {
+        self.actions.forEach {
+            $0.phase(string)
+        }
+    }
     func createDirectory(url: URL) throws {
         try self.actions.forEach {
             try $0.createDirectory(url: url)
@@ -240,34 +371,6 @@ class CompositeAction: SystemAction {
         }
     }
 }
-
-
-// MARK: Utility Functions
-
-///// Create a file at a given path.
-/////
-///// This will overwrite existing files.
-///// - Parameters:
-/////   - file: fileURL to create
-/////   - content: Content of file
-///// - Throws: any problems in creating file.
-//func createFile(file: URL, content: String) throws {
-//    let fm = FileManager.default
-//    try? fm.removeItem(at: file)
-//    try content.write(to: file, atomically: false, encoding: .utf8)
-//}
-//
-///// Create a file at a given path.
-/////
-///// This will overwrite existing files.
-///// - Parameters:
-/////   - file: fileURL to create
-/////   - contentBuilder: A closure that returns the content to write into the file.
-///// - Throws: any problems in creating file.
-//func createFile(file: URL, _ contentBuilder: ()->String) throws {
-//    let content = contentBuilder()
-//    try createFile(file: file, content: content)
-//}
 
 // MARK: Version Extensions
 extension Version {
