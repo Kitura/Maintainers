efadbe7ce598ced93766dbc82cc96d17a1192335
Danny Sung <danny@dannysung.com>
2021-04-03 22:16:25 -0700

    docker build:ensure aliases get pushed; build Ubutu 16.04 (xenial); add license and author info

--- docker/build.swift
+++ docker/build.swift
@@ -1,4 +1,19 @@
 #!/usr/bin/swift sh
+//  Created by Danny Sung on 2021-03-14
+//
+// 	Licensed under the Apache License, Version 2.0 (the "License");
+// 	you may not use this file except in compliance with the License.
+// 	You may obtain a copy of the License at
+//
+// 	http://www.apache.org/licenses/LICENSE-2.0
+//
+// 	Unless required by applicable law or agreed to in writing, software
+// 	distributed under the License is distributed on an "AS IS" BASIS,
+// 	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// 	See the License for the specific language governing permissions and
+// 	limitations under the License.
+//
+
 import Version          // @mrackwitz
 import Foundation
 import ArgumentParser   // https://github.com/apple/swift-argument-parser.git
@@ -16,7 +31,7 @@ let SwiftAliases = [
     ]
 
 /// Ubuntu Versions to build
-let UbuntuVersions = [ "18.04", "20.04" ] // first in this list will be the "default"
+let UbuntuVersions = [ "16.04", "18.04", "20.04" ] // first in this list will be the "default"
 
 /// CentOS Versions to build
 let CentosVersions = [ "8", "7" ]
@@ -29,10 +44,10 @@ struct BuildCommand: ParsableCommand {
 
     @Flag(name: .shortAndLong, help: "Enable verbose mode")
     var verbose: Bool = false
-        
+
     @Flag(inversion: .prefixedEnableDisable, help: "Build docker images")
     var build: Bool = false
-    
+
     @Flag(inversion: .prefixedEnableDisable, help: "Push docker images to registry")
     var push: Bool = false
 
@@ -63,7 +78,7 @@ struct BuildCommand: ParsableCommand {
 
     @Flag(name: [.customLong("registry-password-stdin")], help: "Read registry password from stdin")
     var enableReadRegistryPasswordFromStdin: Bool = false
-    
+
     mutating func run() throws {
         var registryPasswordFromStdin: String? = nil
         var targetsToBuild: [DockerCreator] = []
@@ -158,6 +173,7 @@ struct BuildCommand: ParsableCommand {
             actions.section("Push docker image to public registry")
             
             try targetsPublic.push()
+            try targetsToAlias.push()
         }
         
         // Support pushing to private registry
