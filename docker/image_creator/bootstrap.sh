c91f43122de93035ac3e097bd19aba550316a2a8
Danny Sung <danny@dannysung.com>
2022-02-05 07:08:47 -0800

    Add swift-sh and clone project directory to bootstrap

--- docker/image_creator/bootstrap.sh
+++ docker/image_creator/bootstrap.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 # A script to use to get a Linux environment capable of running Swift
 
-podman build -t kitura-dev-bootstrap:latest -<<EOF
+podman build -t kitura-docker-bootstrap:latest -<<EOF
 FROM swift:latest
 
 RUN apt-get update && apt-get install -y \
     vim \
+    docker.io \
     && rm -rf /var/lib/apt/lists/*
-RUN git clone https://github.com/mxcl/swift-sh.git && (cd swift-sh && swift build -c release)
-COPY swift-sh/.build/release/swift-sh /usr/local/bin/swift-sh
+RUN git clone https://github.com/mxcl/swift-sh.git && \
+    (cd swift-sh && swift build -c release) && \
+    cp swift-sh/.build/release/swift-sh /usr/local/bin/swift-sh && \
+    rm -rf swift-sh
+RUN mkdir /project && cd /project
+RUN cd /project && \
+    git clone https://github.com/Kitura/Maintainers.git && \
+    (cd Maintainers/docker/image_creator && ./build.swift -h)
 
 WORKDIR /project
 ENTRYPOINT ["/bin/bash"]
